6a800e8333f2ae4ca44c41428d028954769e0403
xxiicode <leandrobilokapic@gmail.com>
2024-09-19 16:00:23 +0200

    Agregar archivos de proyecto.

---
+++ TP1-DSOO-Grupo14/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TP1_DSOO_Grupo14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Grupo 14!");
+        }
+    }
+}
